30ea3a09962485824b8906d0f2c31059d1b94ed1
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2025-01-06 22:56:04 +0100

    commit all

---
+++ AdminDashBoardProject/admin-dashboard/src/data/mockData.js
@@ -0,0 +1,34 @@
+export const mockDataTeam = [
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    age: 35,
+    phone: "(666)121-5454",
+    access: "admin",
+  },
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    age: 35,
+    phone: "(666)121-5454",
+    access: "admin",
+  },
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    age: 35,
+    phone: "(666)121-5454",
+    access: "admin",
+  },
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    age: 35,
+    phone: "(666)121-5454",
+    access: "admin",
+  },
+];
